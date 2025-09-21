4052e0e111137a6b636fea51ede9dda755e681ec
Sayak Roy <sayakroy058@gmail.com>
2025-09-21 20:53:58 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -4,4 +4,10 @@ public static void main(String[] args)
 {
 System.out.println("Exploring git & GitHub!");
 }
+public static void fun(){
+    int a =10;
+    int b =20;
+    int sum = a+b;
+    System.out.println(sum);
+}
 }
